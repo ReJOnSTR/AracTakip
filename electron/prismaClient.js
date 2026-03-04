e6b4bef10683b303a3f322e33a1f0c8c3c20d8ae
rejons <hallsak55@gmail.com>
2026-03-04 09:39:38 +0300

    Fix: Finalize Prisma native electron output to electron/prisma/client bypassing ASAR unbundling issues completely

--- electron/prismaClient.js
+++ electron/prismaClient.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client/index');
+const { PrismaClient } = require('./prisma/client');
 const { PrismaBetterSqlite3 } = require('@prisma/adapter-better-sqlite3');
 const { app } = require('electron');
 const path = require('path');
